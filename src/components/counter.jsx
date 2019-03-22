db3e05776a4da2833154365a4fa87a37a667a8ab
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-03-22 17:37:32 +0200

    Adds dynamic classes of badge and button, and event handler to update counter state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,10 +8,25 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
