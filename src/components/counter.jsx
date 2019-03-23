b5b45e2043c097c748940ad5497198b29ed5d2f0
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-03-23 18:04:09 +0200

    Modifies increment handler to pass argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -19,7 +19,8 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
