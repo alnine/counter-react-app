6249b50f5e01badf7e16f0ee7995adfee3548766
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-03-28 17:47:28 +0200

    Adds example of stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,3 +16,21 @@ class NavBar extends Component {
 }
 
 export default NavBar;
+
+//  Stateless Functional Component Example
+/*
+    const NavBar = (props) => {
+      return (
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {props.totalCounters}
+            </span>
+          </a>
+        </nav>
+      );
+    }
+
+    export default NavBar;
+*/
