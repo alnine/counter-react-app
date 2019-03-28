62cf0acf340367ca997718488d18d32cf2085380
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-03-28 18:23:34 +0200

    Stateless functional component is actived

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//  Stateless Functional Component Example
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
 
-//  Stateless Functional Component Example
 /*
-    const NavBar = (props) => {
-      return (
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge badge-pill badge-secondary">
-              {props.totalCounters}
-            </span>
-          </a>
-        </nav>
-      );
+    class NavBar extends Component {
+      render() {
+        return (
+          <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+              Navbar{" "}
+              <span className="badge badge-pill badge-secondary">
+                {this.props.totalCounters}
+              </span>
+            </a>
+          </nav>
+        );
+      }
     }
 
     export default NavBar;
